d5006153f9ec595aeb584940e4ca7ceebafb0a33
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-12 04:54:07 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,19 +5,25 @@ class SongsController < ApplicationController
   end
   
   def new 
-    
+    @song = Song.new
   end
   
   def create 
+    @song = Song.new(song_params(:name, :artist_id, :genre_id))
+    @song.save 
     
+    redirect_to song_path(@song)
   end
   
   def edit 
-    
+    set_song
   end
   
   def update 
+    set_song
+    @song.update(song_params(:name, :artist_id, :genre_id))
     
+    redirect_to song_path(@song)
   end
   
   private
