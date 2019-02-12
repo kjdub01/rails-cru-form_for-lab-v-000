b06c9746b6036251065fa295e37b08104ea51502
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-12 04:51:50 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,32 @@
 class SongsController < ApplicationController
+  
+  def show 
+    set_song
+  end
+  
+  def new 
+    
+  end
+  
+  def create 
+    
+  end
+  
+  def edit 
+    
+  end
+  
+  def update 
+    
+  end
+  
+  private
+  
+  def set_song 
+    @song = SOng.find(params[:id])
+  end
+  
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
 end
