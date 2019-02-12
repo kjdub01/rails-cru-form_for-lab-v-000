78fbb0ad5961d886ef89194256aeb9e116c28875
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-12 04:59:32 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,7 +29,7 @@ class SongsController < ApplicationController
   private
   
   def set_song 
-    @song = SOng.find(params[:id])
+    @song = Song.find(params[:id])
   end
   
   def song_params(*args)
